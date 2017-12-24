11c76337c8f80f61c4fe99a1e144e6c3807b769c
Viktor Bogutskii <info@siteograf.com>
2017-12-24 10:45:27 -0800

    fixed nodemon dev script, refactor

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import productRouter from './src/routes/routes';
+import productRouter from './routes/routes';
 
 const app = express();
 app.disable('x-powered-by');
@@ -39,7 +39,7 @@ app.use((req, res, next) => { // eslint-disable-line consistent-return
   next();
 });
 
-app.use('/product', productRouter);
+app.use('/products', productRouter);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
