274f6cb0d09491afc2d63baf3a39e26e278a7043
Viktor Bogutskii <info@siteograf.com>
2017-12-27 20:36:28 -0800

    file upload refactor

---
+++ server/src/modules/fileUpload/fileUpload.js
@@ -0,0 +1,32 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpg'
+    || file.mimetype === 'image/jpeg'
+    || file.mimetype === 'image/gif'
+    || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5 Mb
+  },
+  fileFilter,
+});
+
+export default upload;
--- server/src/modules/product/productRoutes.js
+++ server/src/modules/product/productRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import multer from 'multer';
+import upload from './../fileUpload/fileUpload';
+
 import {
   productGetAll,
   productCreate,
@@ -9,35 +10,6 @@ import {
 } from './productControllers';
 
 const router = Router();
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename(req, file, cb) {
-    cb(null, new Date().toISOString() + file.originalname);
-  },
-});
-
-const fileFilter = (req, file, cb) => {
-  // reject a file
-  if (file.mimetype === 'image/jpg'
-    || file.mimetype === 'image/jpeg'
-    || file.mimetype === 'image/gif'
-    || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5, // 5 Mb
-  },
-  fileFilter,
-});
 
 router.get('/', productGetAll);
 router.post('/', upload.single('image'), productCreate);
