1af0a8ba4b9511ea7a83f48fa10a4ce88fc29d7f
Viktor Bogutskii <info@siteograf.com>
2018-01-03 16:16:54 -0800

    wrap get http method

--- client/src/modules/httpRequest/httpMethods.js
+++ client/src/modules/httpRequest/httpMethods.js
@@ -2,10 +2,30 @@ import axios from 'axios';
 import Notifications from 'react-notification-system-redux';
 import store from '../../redux/store';
 
-export default function post(URL, data) {
+export function post(URL, data) {
   return axios.post(URL, data)
     .then((res) => {
-      console.log(res);
+      store.dispatch(Notifications.removeAll()); // Removes all notifications
+      store.dispatch(Notifications.success({
+        title: res.data.message.text,
+        autoDismiss: 0,
+      }));
+      return res;
+    })
+    .catch((error) => {
+      if (error.response) {
+        store.dispatch(Notifications.error({
+          title: error.response.data.message.text,
+          autoDismiss: 0,
+        }));
+      }
+      return error;
+    });
+}
+
+export function get(URL, data) {
+  return axios.het(URL, data)
+    .then((res) => {
       store.dispatch(Notifications.removeAll()); // Removes all notifications
       store.dispatch(Notifications.success({
         title: res.data.message.text,
--- client/src/modules/user/_actions/userActions.js
+++ client/src/modules/user/_actions/userActions.js
@@ -1,7 +1,4 @@
-import axios from 'axios';
-import Notifications from 'react-notification-system-redux';
-
-import post from '../../httpRequest/httpMethods';
+import { get, post } from '../../httpRequest/httpMethods';
 
 export function userRegister(email, password) {
   return dispatch =>
@@ -20,21 +17,12 @@ export function userRegister(email, password) {
 
 export function getAllUsers() {
   return dispatch =>
-    axios.get('user')
+    get('user')
       .then((res) => {
-        console.log(res);
         dispatch({
           type: 'USER_LIST',
           payload: res.data,
         });
-      })
-      .then(() => {
-        dispatch(Notifications.success({ title: 'OK', message: 'ok' }));
-      })
-      .catch((error) => {
-        if (error.response) {
-          dispatch(Notifications.error({ title: 'Erorr', message: error.response.data.message.text }));
-        }
       });
 }
 
