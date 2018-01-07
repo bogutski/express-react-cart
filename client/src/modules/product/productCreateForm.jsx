bc71660ad6ffe58ccfd57c8ea1db4c77466adeb0
Viktor Bogutskii <info@siteograf.com>
2018-01-06 17:14:31 -0800

    product form

--- client/src/modules/product/productCreateForm.jsx
+++ client/src/modules/product/productCreateForm.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Button, Form } from 'reactstrap';
 import { Field, reduxForm } from 'redux-form';
 import { TextField } from './../form/form';
-import { email as validEmail, minLength5, required } from './../form/validators';
+import { required, number } from './../form/validators';
 import { productCreate } from './_actions/productActions';
 
 class ProductCreateForm extends Component {
@@ -16,8 +16,11 @@ class ProductCreateForm extends Component {
   formSubmit(e) {
     e.preventDefault();
 
-    const email = this.props.userRegisterForm.values.email;
-    const password = this.props.userRegisterForm.values.password;
+    const product = {
+      title: this.props.productForm.values.title,
+      price: this.props.productForm.values.price,
+    };
+
     this.props.productCreate(product);
   }
 
@@ -26,26 +29,26 @@ class ProductCreateForm extends Component {
       <Form onSubmit={this.formSubmit}>
         <h3>Product Create</h3>
         <Field
-          name="email"
+          name="title"
           type="text"
-          placeholder="Email"
+          placeholder="Title"
           component={TextField}
-          validate={[required, validEmail]}
+          validate={[required]}
         />
 
         <Field
-          placeholder="Password"
-          name="password"
+          name="price"
+          type="text"
+          placeholder="Price"
           component={TextField}
-          type="password"
-          validate={[required, minLength5]}
+          validate={[required, number]}
         />
 
         <Button
           type="submit"
           color="primary"
-          disabled={this.props.productCreateForm && {}.hasOwnProperty.call(this.props.productCreateForm, 'syncErrors')}
-        >Create product
+          disabled={this.props.productForm && {}.hasOwnProperty.call(this.props.productForm, 'syncErrors')}
+        >Save
         </Button>
       </Form>
     );
@@ -53,7 +56,7 @@ class ProductCreateForm extends Component {
 }
 
 const mapStateToProps = state => ({
-  productCreateForm: state.form.productCreate,
+  productForm: state.form.productCreate,
 });
 
 const mapDispatchToProps = dispatch => ({
@@ -63,5 +66,5 @@ const mapDispatchToProps = dispatch => ({
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
-  reduxForm({ form: 'productCreate' }),
+  reduxForm({ form: 'product' }),
 )(ProductCreateForm);
--- client/src/modules/product/productIndex.jsx
+++ client/src/modules/product/productIndex.jsx
@@ -1,46 +1,14 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import _ from 'lodash';
-import ReactTable from 'react-table';
-import { getAllProducts } from './_actions/productActions';
-import { withRouter } from 'react-router-dom';
+import ProductCreateForm from './productCreateForm';
 
 class ProductIndex extends Component {
-  componentDidMount() {
-    if (_.isEmpty(this.props.userList)) {
-      // this.props.getAllUsers();
-    }
-  }
-
-  columns() {
-    return [{
-      Header: 'Id',
-      accessor: '_id', // String-based value accessors!
-    }, {
-      Header: 'Email',
-      accessor: 'email',
-    }];
-  }
-
   render() {
     return (
       <div>
-        <ReactTable
-          data={this.props.userList}
-          columns={this.columns()}
-          minRows={0}
-        />
+        <ProductCreateForm />
       </div>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  // userList: state.user.userList,
-});
-
-const mapDispatchToProps = dispatch => ({
-  // getAllUsers: () => dispatch(getAllUsers()),
-});
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductIndex));
+export default ProductIndex;
