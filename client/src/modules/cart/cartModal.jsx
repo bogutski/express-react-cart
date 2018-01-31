af3a08b2bbe9ea3951d795e6b7d623298b76b53c
Viktor Bogutskii <info@siteograf.com>
2018-01-30 21:07:12 -0800

    product table in cart

--- client/src/modules/cart/cartModal.jsx
+++ client/src/modules/cart/cartModal.jsx
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { connect } from 'react-redux';
+import ReactTable from 'react-table';
 import { cartVisibleToggle } from './_actions/cartActions';
 
 class CartModal extends Component {
+  columns() {
+    return [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Price',
+        accessor: 'price',
+      },
+      {
+        Header: 'Qt',
+        accessor: 'qt',
+      },
+      {
+        Header: 'Subtotal',
+        accessor: 'total',
+      },
+      {
+        Header: 'Actions',
+        id: 'act',
+        accessor: el => (
+          <div>
+            <Button color="link" size="sm" onClick={() => this.delete(el._id)}>Delete</Button>
+          </div>
+        ),
+      },
+    ];
+  }
+
   render() {
     return (
       <Modal
+        className="modal-lg"
         isOpen={this.props.cartVisible}
         toggle={this.props.cartVisibleToggle}
       >
         <ModalHeader toggle={this.props.cartVisibleToggle}>Cart title</ModalHeader>
         <ModalBody>
-          Cart table
+
+          <ReactTable
+            className="light"
+            resizable={false}
+            data={this.props.cart.cart}
+            columns={this.columns()}
+            minRows={0}
+            defaultPageSize={30}
+            showPagination={this.props.cart.cart.length > 30}
+          />
+
+          {this.props.cart.totalCount} {this.props.cart.totalCount > 1 ? 'items' : 'item'}
+          {' '}
+          ${this.props.cart.totalPrice}
+
         </ModalBody>
         <ModalFooter>
           <Button
+            outline
             color="secondary"
             onClick={this.props.cartVisibleToggle}
           >Continue chopping
           </Button>
-          <Button color="primary" >Checkout</Button>{' '}
+          {' '}
+          <Button color="primary">Checkout</Button>
         </ModalFooter>
       </Modal>
     );
@@ -30,6 +78,7 @@ class CartModal extends Component {
 
 const mapStateToProps = state => ({
   cartVisible: state.cart.cartVisible,
+  cart: state.cart,
 });
 
 const mapDispatchToProps = dispatch => ({
