e873f734a023bae2c97307b79179fb330a02a6b1
Viktor Bogutskii <info@siteograf.com>
2018-02-01 23:21:02 -0800

    updatet tests: globals

---
+++ testing/data/globals.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: '',
+  password: '',
+  title: {
+    home: 'React App',
+  },
+  url: 'http://localhost:3000',
+};
--- testing/tests/home.js
+++ testing/tests/home.js
@@ -1,9 +1,19 @@
+const globals = require('../data/globals');
+
 module.exports = {
-  'Home page': function (client) {
+  'Home page': (client) => {
     client
-      .url('http://localhost:3000/')
+      .url(globals.url)
       .waitForElementVisible('#root', 100)
-      .assert.title('React App')
+      .assert.title(globals.title.home)
+      .end();
+  },
+
+  'Catalog page': (client) => {
+    client
+      .url(globals.url)
+      .waitForElementVisible('#root', 100)
+      .assert.title(globals.title.home)
       .end();
   },
 };
