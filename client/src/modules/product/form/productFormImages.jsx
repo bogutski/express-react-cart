4ea9b4afe5ce8783377fb2e8c9f261f7056a99cb
Viktor Bogutskii <info@siteograf.com>
2018-02-20 23:59:06 -0800

    WIP image thumbs resort

--- client/src/modules/product/form/productFormImages.jsx
+++ client/src/modules/product/form/productFormImages.jsx
@@ -2,32 +2,49 @@ import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { arrayMove, SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 import Img from '../../utils/img/img';
+
+const DragHandle = SortableHandle(() => <div className="svg-icon hamburger w30 h30" />);
+
+const SortableItem = SortableElement(({ value }) => (
+  <div className="d-flex">
+    <DragHandle />
+    <Img pid={value.pid} h={50} />
+    {value.pid}
+    <span>Delete</span>
+  </div>
+));
+
+const SortableList = SortableContainer(({ items }) => (
+  <div>
+    {items.map((value, index) => (
+      <SortableItem key={`${value.pid}`} index={index} value={value} />
+    ))}
+  </div>
+));
 
 class ProductFormImages extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
+    };
   }
 
-  formSubmit(e) {
-    e.preventDefault();
-
-    const data = {
-      ...this.props.productForm.values,
-    };
+  onSortEnd({ oldIndex, newIndex }) {
+    this.setState({
+      items: arrayMove(this.state.items, oldIndex, newIndex),
+    });
   }
 
   render() {
     return (
-      <ul>
-        {_.get(this.props, 'productForm.values.image', [])
-          .map(el => (
-            <li key={el.pid}>
-              <Img pid={el.pid} h={50} />
-              <span>Delete</span>
-            </li>))
-        }
-      </ul>
+      <SortableList
+        items={_.get(this.props, 'productForm.values.image', [])}
+        onSortEnd={() => this.onSortEnd}
+        useDragHandle
+      />
     );
   }
 }
