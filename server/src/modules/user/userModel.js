4570512c0d9c6f8bd8ad1ef9c75aee4f6db6a3de
Viktor Bogutskii <info@siteograf.com>
2018-02-26 22:55:50 -0800

    shipping: server side

--- server/src/modules/user/userModel.js
+++ server/src/modules/user/userModel.js
@@ -8,7 +8,14 @@ const userSchema = mongoose.Schema({
     unique: true,
     match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
   },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+  },
+  shipping: {
+    type: Array,
+    required: false,
+  },
 });
 
 export default mongoose.model('User', userSchema);
--- server/src/modules/user/userRoutes.js
+++ server/src/modules/user/userRoutes.js
@@ -8,13 +8,19 @@ import {
   userDeleteById,
 } from './userControllers';
 
+import { updateShipping } from './profile/profileControllers';
+
 const router = Router();
 
+// USER
 router.get('/', userCheckAuth, userGetAll);
 router.post('/', userCreate);
 router.post('/login', userLogin);
 router.get('/:userId', userCheckAuth, userGetById);
 router.delete('/:userId', userCheckAuth, userDeleteById);
+
+// PROFILE
+router.patch('/shipping/:userId', updateShipping);
 
 // Not active
 // router.patch('/:userId', userUpdateById);
