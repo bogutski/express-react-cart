435edebea88050189acf4df409b582c6c0a26421
Viktor Bogutskii <info@siteograf.com>
2018-03-16 23:56:46 -0700

    footer fix

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <footer className="pt-4 mt-5 pt-md-5 border-top">
+      <footer className="pt-5 mt-5 pb-5">
         <div className="container-fluid">
           <div className="row">
             <div className="col-12 col-md">
@@ -11,38 +11,36 @@ class Footer extends Component {
                 className="mb-2"
                 src="favicon.png"
                 alt=""
-                width="24"
-                height="24"
+                width="48"
+                height="48"
               />
               <small className="d-block mb-3 text-muted">© 2017-2018</small>
             </div>
             <div className="col-6 col-md">
               <h5>Features</h5>
               <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Cool stuff</a></li>
-                <li><a className="text-muted" href="#">Random feature</a></li>
-                <li><a className="text-muted" href="#">Team feature</a></li>
-                <li><a className="text-muted" href="#">Stuff for developers</a></li>
-                <li><a className="text-muted" href="#">Another one</a></li>
-                <li><a className="text-muted" href="#">Last time</a></li>
+                <li><span className="text-muted">Cool stuff</span></li>
+                <li><span className="text-muted">Random feature</span></li>
+                <li><span className="text-muted">Team feature</span></li>
+                <li><span className="text-muted">Stuff for developers</span></li>
               </ul>
             </div>
             <div className="col-6 col-md">
               <h5>Resources</h5>
               <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Resource</a></li>
-                <li><a className="text-muted" href="#">Resource name</a></li>
-                <li><a className="text-muted" href="#">Another resource</a></li>
-                <li><a className="text-muted" href="#">Final resource</a></li>
+                <li><span className="text-muted">Resource</span></li>
+                <li><span className="text-muted">Resource name</span></li>
+                <li><span className="text-muted">Another resource</span></li>
+                <li><span className="text-muted">Final resource</span></li>
               </ul>
             </div>
             <div className="col-6 col-md">
               <h5>About</h5>
               <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="#">Team</a></li>
-                <li><a className="text-muted" href="#">Locations</a></li>
-                <li><a className="text-muted" href="#">Privacy</a></li>
-                <li><a className="text-muted" href="#">Terms</a></li>
+                <li><span className="text-muted">Team</span></li>
+                <li><span className="text-muted">Locations</span></li>
+                <li><span className="text-muted">Privacy</span></li>
+                <li><span className="text-muted">Terms</span></li>
               </ul>
             </div>
           </div>
