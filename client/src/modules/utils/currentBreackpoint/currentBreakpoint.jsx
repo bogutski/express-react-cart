32aee14121c0ef1327908780e909f9659c42cbde
Viktor Bogutskii <info@siteograf.com>
2018-03-18 16:36:46 -0700

    current breakpoint

---
+++ client/src/modules/utils/currentBreackpoint/currentBreakpoint.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.scss';
+
+class CurrentBreakpoint extends Component {
+  render() {
+    return (
+      <div className="bootSizes">
+        <div className="displaySize only-xs">XS</div>
+        <div className="displaySize only-sm">SM</div>
+        <div className="displaySize only-md">MD</div>
+        <div className="displaySize only-lg">LG</div>
+        <div className="displaySize only-xl">XL</div>
+      </div>
+    );
+  }
+}
+
+export default CurrentBreakpoint;
