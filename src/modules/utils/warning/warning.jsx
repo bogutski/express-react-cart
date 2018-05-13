710b91d14f27c5440412e1e14c9bd0e36898af70
Viktor Bogutskii <info@siteograf.com>
2018-05-12 22:47:45 -0700

    human code errors

--- src/modules/utils/warning/warning.jsx
+++ src/modules/utils/warning/warning.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const code = {
+  500: 'Server error',
+};
+
 class Warning extends Component {
   render() {
     return (
       <ul>
         {this.props.warnings.map(el => (
-          <li><h2>{el}</h2></li>
+          <li><h2>{el} {code[el]}</h2></li>
         ))}
       </ul>
     );
